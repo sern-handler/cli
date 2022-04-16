82234f44de440361a8062899489c39be5e71efdb
EvolutionX <jeyush2005@gmail.com>
2022-04-16 11:54:40 +0530

    chore: update to index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 #!/usr/bin/env node
 const yargs = require('yargs');
-
-yargs.scriptName('sern');
-yargs.command({
-	command: 'init',
-	describe: 'Initialize a new project',
-	builder: {
-		name: {
-			describe: 'Project name',
-			demandOption: true,
-			type: 'string',
-		},
-	},
-	handler: function (argv) {
-		console.log('Initializing a new project...');
-		console.log('Project name: ' + argv.name);
-	},
-}).argv;
+const usage = '\nUsage: sern init';
+yargs
+	.usage(usage)
+	.option('i', {
+		describe: 'Set up basic project without any customizations',
+		boolean: true,
+	})
+	.help(true).argv;
