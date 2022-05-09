1265224bb9f93cb104915be50c1c2ea1e3924955
qxb3 <toolong972@gmail.com>
2022-05-10 01:07:46 +0800

    fix: prettier things

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,6 +1,3 @@
 export function help() {
-	console.log(
-		'This is sern cli help section\n\n' +
-		'Fill me up later!'
-	)
+	console.log('This is sern cli help section\n\n' + 'Fill me up later!');
 }
