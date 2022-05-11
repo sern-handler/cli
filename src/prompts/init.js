c5b45aab59d080bbc4254ffb17c3a8c951b58e08
EvolutionX <jeyush2005@gmail.com>
2022-05-11 09:41:54 +0530

    chore: removed deprecated stuff

--- src/prompts/init.js
+++ src/prompts/init.js
@@ -1,26 +1,5 @@
 import { blueBright } from 'colorette';
 
-// TODO refactor the intents stuff and add more questions
-const Intents = [
-	'DIRECT_MESSAGES',
-	'DIRECT_MESSAGE_REACTIONS',
-	'DIRECT_MESSAGE_TYPING',
-	'GUILDS',
-	'GUILD_BANS',
-	'GUILD_EMOJIS_AND_STICKERS',
-	'GUILD_INTEGRATIONS',
-	'GUILD_INVITES',
-	'GUILD_MEMBERS',
-	'GUILD_MESSAGES',
-	'GUILD_MESSAGE_REACTIONS',
-	'GUILD_MESSAGE_TYPING',
-	'GUILD_PRESENCES',
-	'GUILD_SCHEDULED_EVENTS',
-	'GUILD_VOICE_STATES',
-	'GUILD_VOICE_STATES',
-	'GUILD_WEBHOOKS',
-].map((i, j) => ({ title: i, value: j, short: `${j}` })); //! bad way
-
 export const lang = {
 	message: 'What language do you want the project to be in?',
 	name: 'lang',
@@ -40,19 +19,6 @@ export const lang = {
 	],
 };
 
-/**
- * 	@deprecated
- *  ! Will be removed !
- */
-export const intent = {
-	message: 'What intents do you want to keep?',
-	type: 'multiselect',
-	name: 'intents',
-	choices: Intents,
-	min: 1,
-	max: 16, //? 17 is max, i dont allow all
-};
-
 export const default_prefix = {
 	message:
 		'What is the default prefix for your bot? Type "none" if it is completely Application-Command based',
@@ -60,20 +26,6 @@ export const default_prefix = {
 	name: 'prefix',
 	type: 'text',
 	initial: '!',
-};
-
-export const token = {
-	message: 'What is your bot token? Type "no" to skip',
-	name: 'token',
-	type: 'password',
-	validate: (/** @type {string} */ token) => {
-		if (token === 'no') return true;
-		return /(?<mfaToken>mfa\.[a-z0-9_-]{20,})|(?<basicToken>[a-z0-9_-]{23,28}\.[a-z0-9_-]{6,7}\.[a-z0-9_-]{27})/i.test(
-			token
-		)
-			? true
-			: 'Invalid token';
-	},
 };
 
 export const main_dir = {
