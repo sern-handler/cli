db709f388b81a83e61d9377e3ea46aef00c315bd
Allyedge <alimarslank@gmail.com>
2022-05-11 20:01:26 +0200

    feat: set git init to true and package manager to npm by default

--- src/commands/init.js
+++ src/commands/init.js
@@ -50,77 +50,84 @@ export async function init({ flags }) {
 		await npm();
 	}
 
-/**
- * Initialize a new project, either with prompts or with default values.
- * @param {boolean} isDefault - Whether or not to use default values.
- */
-const initProject = async (isDefault) => {
-	let data;
+	/**
+	 * Initialize a new project, either with prompts or with default values.
+	 * @param {boolean} isDefault - Whether or not to use default values.
+	 */
+	const initProject = async (isDefault) => {
+		let data;
 
-	if (!isDefault) {
-		data = await prompt([name, lang, main_dir, cmds_dir, default_prefix]);
-	} else {
-		const projectName = await prompt([name]);
+		if (!isDefault) {
+			data = await prompt([
+				name,
+				lang,
+				main_dir,
+				cmds_dir,
+				default_prefix,
+			]);
+		} else {
+			const projectName = await prompt([name]);
 
-		data = {
-			name: projectName.name,
-			lang: 'typescript',
-			main_dir: 'src',
-			cmds_dir: 'commands',
-			default_prefix: '!',
-		};
+			data = {
+				name: projectName.name,
+				lang: 'typescript',
+				main_dir: 'src',
+				cmds_dir: 'commands',
+				default_prefix: '!',
+			};
+		}
+
+		if (!isDefault && Object.keys(data).length < 5) process.exit(1);
+
+		await cloneRepo(data.lang, data.name);
+
+		git_init ? await git(data) : console.log(`Skipping git init...\n`);
+
+		let choice = '';
+
+		if (pm === 'both') {
+			const chosen = await prompt([which_manager]);
+			choice = chosen.manager;
+		} else choice = pm;
+
+		await installDeps(choice, data.name);
+		await editMain(data.name);
+		await editDirs(data.main_dir, data.cmds_dir, data.name);
+	};
+
+	/**
+	 * It initializes git
+	 * @param data - The data object that contains the name of the project.
+	 */
+	async function git(data) {
+		const spin = ora({
+			text: 'Initializing git...',
+			spinner: 'aesthetic',
+		}).start();
+
+		const exe = await execa('git', ['init', data.name]);
+
+		await wait(300);
+
+		if (!exe || exe?.failed) {
+			spin.fail(
+				`${redBright('Failed')} to initialize git!` +
+					'\nMaybe you should run git init?'
+			);
+			process.exit(1);
+		}
+
+		spin.succeed('Git initialized!');
 	}
 
-	if (!isDefault && Object.keys(data).length < 5) process.exit(1);
-
-	await cloneRepo(data.lang, data.name);
-
-	git_init ? await git(data) : console.log(`Skipping git init...\n`);
-
-	let choice = '';
-
-	if (pm === 'both') {
-		const chosen = await prompt([which_manager]);
-		choice = chosen.manager;
-	} else choice = pm;
-
-	await installDeps(choice, data.name);
-	await editMain(data.name);
-	await editDirs(data.main_dir, data.cmds_dir, data.name);
-};
-
-/**
- * It initializes git
- * @param data - The data object that contains the name of the project.
- */
-async function git(data) {
-	const spin = ora({
-		text: 'Initializing git...',
-		spinner: 'aesthetic',
-	}).start();
-
-	const exe = await execa('git', ['init', data.name]);
-
-	await wait(300);
-
-	if (!exe || exe?.failed) {
-		spin.fail(
-			`${redBright('Failed')} to initialize git!` +
-				'\nMaybe you should run git init?'
-		);
-		process.exit(1);
+	/**
+	 * Wait for a specified number of milliseconds, then return a promise that resolves to undefined.
+	 * @param {number} ms - The number of milliseconds to wait.
+	 * @returns A function that takes a single argument, ms, and returns a promise that resolves after ms
+	 * milliseconds.
+	 */
+	async function wait(ms) {
+		const wait = (await import('util')).promisify(setTimeout);
+		return wait(ms);
 	}
-
-	spin.succeed('Git initialized!');
-}
-
-/**
- * Wait for a specified number of milliseconds, then return a promise that resolves to undefined.
- * @param {number} ms - The number of milliseconds to wait.
- * @returns A function that takes a single argument, ms, and returns a promise that resolves after ms
- * milliseconds.
- */
-async function wait(ms) {
-	const wait = (await import('util')).promisify(setTimeout);
-	return wait(ms);
 }
