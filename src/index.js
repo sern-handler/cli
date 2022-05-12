1ed35fb82278f2442556bd09433a805d1541eb3e
xxDeveloper <77380166+Murtatrxx@users.noreply.github.com>
2022-05-12 18:16:15 +0300

    feat: Added version command to index.js & changed export type to default

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
-import { init } from './commands/init.js';
-import { help } from './commands/help.js';
+import init from './commands/init.js';
+import help from './commands/help.js';
+import version from './commands/version.js';
 
 const regex = /(?<=--|-)\w+/gm;
 const rawArgs = process.argv.slice(2);
@@ -19,6 +20,7 @@ const commands = new Map([
 	['help', help],
 	['', help],
 	['init', init],
+        ['version', version]
 ]);
 
 const found = commands.get(cmdName);
