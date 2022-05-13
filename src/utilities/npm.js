e5f607e99875e105cbb148cab3ed1bbc3771ae35
Allyedge <66177622+Allyedge@users.noreply.github.com>
2022-05-13 07:39:35 +0200

    feat: add default settings for the -y flag (#19)
    * feat: add default settings for the -y flag
    
    Signed-off-by: Allyedge <alimarslank@gmail.com>
    
    * refactor: add documentation comments to initProject
    
    * feat: set git init to true and package manager to npm by default
    
    * style: fix typo in npmInit
    
    * Update src/commands/init.js
    
    Co-authored-by: Evo <85353424+EvolutionX-10@users.noreply.github.com>
    
    * Update src/commands/init.js
    
    Co-authored-by: Evo <85353424+EvolutionX-10@users.noreply.github.com>
    
    * refactor: remove unnecessary isDefault checks
    
    * refactor(init): cleaned up code and made a simple flow
    
    * docs: update jsdoc
    
    * style: fix prettier
    
    * feat: set git init to true and package manager to npm by default
    
    * style: fix typo in npmInit
    
    * Update src/commands/init.js
    
    Co-authored-by: Evo <85353424+EvolutionX-10@users.noreply.github.com>
    
    * Update src/commands/init.js
    
    Co-authored-by: Evo <85353424+EvolutionX-10@users.noreply.github.com>
    
    * feat: add default settings for the -y flag
    
    Signed-off-by: Allyedge <alimarslank@gmail.com>
    
    * refactor: add documentation comments to initProject
    
    * feat: set git init to true and package manager to npm by default
    
    * Update src/commands/init.js
    
    Co-authored-by: Evo <85353424+EvolutionX-10@users.noreply.github.com>
    
    * Update src/commands/init.js
    
    Co-authored-by: Evo <85353424+EvolutionX-10@users.noreply.github.com>
    
    * refactor: remove unnecessary isDefault checks
    
    * refactor(init): cleaned up code and made a simple flow
    
    * docs: update jsdoc
    
    * style: fix prettier
    
    * feat: add default settings for the -y flag
    
    Signed-off-by: Allyedge <alimarslank@gmail.com>
    
    * refactor: add documentation comments to initProject
    
    * feat: set git init to true and package manager to npm by default
    
    * refactor: remove unnecessary isDefault checks
    
    * refactor(init): cleaned up code and made a simple flow
    
    * docs: update jsdoc
    
    * style: fix prettier
    
    * chore(s): stuff
    
    Co-authored-by: Evo <85353424+EvolutionX-10@users.noreply.github.com>

--- src/utilities/npm.js
+++ src/utilities/npm.js
@@ -7,14 +7,18 @@ import { execa } from 'execa';
 export async function npm() {
 	const npm = await execa('npm', ['-v']);
 	const npm_version = npm?.stdout;
+
 	const yarn = await execa('yarn', ['-v']);
 	const yarn_version = yarn?.stdout;
+
 	if (npm_version && !yarn_version) {
 		return 'npm';
 	}
+
 	if (!npm_version && yarn_version) {
 		return 'yarn';
 	}
+
 	if (npm_version && yarn_version) {
 		return 'both';
 	}
