321b537a4d9e16f27ed3c8c32fd2f51c5e67d780
Allyedge <alimarslank@gmail.com>
2022-05-29 19:55:20 +0200

    feat(go): start rewrite in go

---
+++ cmd/cli/root.go
@@ -0,0 +1,28 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:     "sern",
+	Short:   "A powerful CLI tool for Sern.",
+	Version: "0.1.0",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Sern CLI")
+	},
+}
+
+func Execute() {
+	rootCmd.Flags().BoolP("help", "h", false, "Help for the Sern CLI.")
+	rootCmd.Flags().BoolP("version", "v", false, "The version of the Sern CLI.")
+	rootCmd.SetVersionTemplate("Sern CLI - Version {{.Version}}\n")
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/sern-handler/cli/cmd/cli"
+
+func main() {
+	cli.Execute()
+}
