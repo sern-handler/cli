d60da9fd3b7baea715e653b6607f735c2380b841
Allyedge <alimarslank@gmail.com>
2022-06-06 10:43:47 +0200

    v0.1.2

--- cmd/cli/root.go
+++ cmd/cli/root.go
@@ -10,7 +10,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:     "sern",
 	Short:   "A powerful CLI tool for Sern.",
-	Version: "0.1.1",
+	Version: "0.1.2",
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Help()
 	},
