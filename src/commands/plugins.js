47e6125ece2f6feaccf7a34b96bf4d8e7f041c36
EvolutionX <jeyush2005@gmail.com>
2022-06-18 12:30:45 +0530

    feat: plugin command!!!

--- src/commands/plugins.js
+++ src/commands/plugins.js
@@ -1,5 +1,33 @@
+import { pluginsQ } from '../prompts/plugin.js';
+import prompts from 'prompts';
+import axios from 'axios';
+import fs from 'fs';
+const { prompt } = prompts;
 /**
  * Installs plugins to project
  * @param flags
  */
-export function plugins(flags) {}
+export async function plugins(flags) {
+	/**
+	 * @type {string[]}
+	 */
+	const e = (await prompt([pluginsQ])).list;
+
+	for await (const url of e) {
+		await download(url);
+	}
+}
+
+async function download(url) {
+	const res = await axios.get(url);
+	const data = res.data;
+	const dir = `${process.cwd()}\\src\\plugins`;
+	const filedir = `${process.cwd()}\\src\\plugins\\${url.split('/').pop()}`;
+	if (!fs.existsSync(dir)) {
+		fs.mkdirSync(dir, { recursive: true });
+	}
+	console.log('done');
+	const file = fs.writeFileSync(filedir, data);
+	console.log('downloaded');
+	return file;
+}
---
+++ src/prompts/plugin.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { getLang } from '../utilities/getLang.js';
+async function gimmechoices() {
+	const lang =
+		(await getLang()) === 'typescript' ? 'TypeScript' : 'JavaScript';
+	const link = `https://api.github.com/repos/sern-handler/awesome-plugins/contents/${lang}`;
+
+	const data = (await axios.get(link)).data;
+	const choices = data.map((e) => ({
+		title: e.name,
+		value: e.download_url,
+	}));
+	return choices;
+}
+
+/**
+ * @type {import('prompts').PromptObject}
+ */
+export const pluginsQ = {
+	name: 'list',
+	type: 'autocompleteMultiselect',
+	message: 'What plugins do you want to install?',
+	choices: await gimmechoices(),
+	min: 1,
+};
