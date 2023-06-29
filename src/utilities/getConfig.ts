8e0ae80242401cdf5f05040b5b9a7ddff24c2c6b
EvolutionX <evolutionx9777@gmail.com>
2023-06-29 23:37:50 +0530

    chore: more debug... who overwrote!
    permalink: http://whatthecommit.com/2e6bbd4fd1a21e16039ce52216c3c0b4

--- src/utilities/getConfig.ts
+++ src/utilities/getConfig.ts
@@ -1,14 +1,13 @@
 import { readFile } from 'node:fs/promises';
 import { findUp } from 'find-up';
-
+import assert from 'node:assert';
 export async function getConfig(): Promise<sernConfig> {
 	const sernLocation = await findUp('sern.config.json');
-
-	if (!sernLocation) throw new Error("Can't find sern.config.json");
+	assert(sernLocation, "Can't find sern.config.json");
 
 	const output = JSON.parse(await readFile(sernLocation, 'utf8')) as sernConfig;
 
-	if (!output) throw new Error("Can't read your sern.config.json.");
+	assert(output, "Can't read your sern.config.json.");
 
 	return output;
 }
