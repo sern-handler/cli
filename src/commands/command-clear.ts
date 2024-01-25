7eeec5e703128851e11d583673d09142579d55df
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2024-01-25 13:57:14 -0600

    add -y flag operation

--- src/commands/command-clear.ts
+++ src/commands/command-clear.ts
@@ -6,20 +6,28 @@ import { readFileSync, writeFile } from 'node:fs'
 import { resolve } from 'node:path'
 import prompts from 'prompts';
 
+const getConfirmation = (args: Record<string,any> ) => {
+    if(args.yes) {
+        return args.yes
+    } else {
+        return prompts({
+            type: 'confirm',
+            name: 'confirmation',
+            message: 'Are you sure you want to delete your application commands?',
+            initial: true
+        }, { onCancel: () => (console.log("Cancelled operation  (￣┰￣*)"), process.exit(0)) })
+        .then(response => response.confirmation);
+    }
+}
 export async function command_clear(args: Record<string,any>) {
     dotenv.configDotenv({ path: args.env || resolve('.env') })
     const token = process.env.token || process.env.DISCORD_TOKEN;
     const appid = process.env.applicationId || process.env.APPLICATION_ID;
     assert(token, 'Could not find a token for this bot in .env or commandline. Do you have DISCORD_TOKEN in env?');
     assert(appid, 'Could not find an application id for this bot in .env or commandline. Do you have APPLICATION_ID in env?');
-
-    const response = await prompts({
-        type: 'confirm',
-        name: 'confirmation',
-        message: 'Are you sure you want to delete your application commands?',
-        initial: true
-      }, { onCancel: () => (console.log("Cancelled operation  (￣┰￣*)"), process.exit(0)) });
-    if (response.confirmation) {
+    
+    const confirmation = await getConfirmation(args);
+    if (confirmation) {
         console.log('Deleting application commands...');
         const rest = Rest.create(appid, token);
         let guildCommands: Record<GuildId, CommandData[]> 
